7a72c55c79c5def0f23c06a7b1f58b1c9224b8bd
kiingz <velazquez.miguel705@gmail.com>
2023-04-03 10:25:43 -0600

    Initial commit

---
+++ src/main/java/net/kiingz/runespellmod/RunespellModClient.java
@@ -0,0 +1,10 @@
+package net.kiingz.runespellmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RunespellModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
